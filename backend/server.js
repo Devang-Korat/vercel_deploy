9d3975f268e98cf4ad030135031f3efa560e5584
Devang-Korat <devangpatel9126@gmail.com>
2026-04-09 19:48:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,3 +20,6 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
